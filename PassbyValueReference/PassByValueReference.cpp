745fb07ae7ca33e704beea4fee8125d056699c53
pn3425 <pranjal.nikhade@gmail.com>
2025-09-02 23:07:31 +0530

    Added Outputs

--- PassbyValueReference/PassByValueReference.cpp
+++ PassbyValueReference/PassByValueReference.cpp
@@ -4,22 +4,22 @@ using namespace std;
 void Value(int x)      //Pass By Value
 {
     x = x + 1;
-    cout<<"Inside Function : "<<" "<<x<<endl;
+    cout<<"Inside Function : "<<" "<<x<<endl;   //6
 }
 
 void Reference(int &x)  //pass By Reference
 {
     x = x + 1;
-    cout<<"Inside Function : "<<" "<<x<<endl;
+    cout<<"Inside Function : "<<" "<<x<<endl;  //11
 }
 
 int main(){
     int a = 5;
     Value(a);
-    cout<<"Outside Function : "<<" "<<a<<endl;
+    cout<<"Outside Function : "<<" "<<a<<endl;  //5
 
     int b = 10;
     Reference(b);
-    cout<<"Outside Function : "<<" "<<b;
+    cout<<"Outside Function : "<<" "<<b;       //11
 
 }
